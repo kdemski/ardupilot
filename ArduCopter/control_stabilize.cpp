5342081643c4b775425f54ae1af1a68ccf09bf61
Kyle Demski <ekdemski@gmail.com>
2016-04-20 16:41:04 -0700

    4

--- ArduCopter/control_stabilize.cpp
+++ ArduCopter/control_stabilize.cpp
@@ -9,6 +9,25 @@
 // stabilize_init - initialise stabilize controller
 bool Copter::stabilize_init(bool ignore_checks)
 {
+    
+   //---------------------------------------------------------------------------------------------------------------------------------------------
+    //kd adc variables  conv factor is 3 since lean angle is in centi degrees so a 30 degree lean would be 3000, and the adc pulls in max 1023, so 3*1023 would give a lean angle of 30 degrees
+    float adcroll = 0, adcpitch = 0, criticalfactor = 1, convfactor = 3;
+
+    //kd Analog In setup, may not be complete
+    AP_HAL::AnalogSource* ch1;
+    AP_HAL::AnalogSource* ch2;
+    AP_HAL::AnalogSource* ch3;
+
+    void setup (void) {
+        hal.console->printf("Starting AP_HAL::AnalogIn test\r\n");
+        //ch = hal.analogin->channel(13);
+        ch1 = hal.analogin->channel(15);
+        ch2 = hal.analogin->channel(13);
+        ch3 = hal.analogin->channel(14);
+    }
+    //---------------------------------------------------------------------------------------------------------------------------------------------    
+    
     // if landed and the mode we're switching from does not have manual throttle and the throttle stick is too high
     if (motors.armed() && ap.land_complete && !mode_has_manual_throttle(control_mode) && (get_pilot_desired_throttle(channel_throttle->control_in) > get_non_takeoff_throttle())) {
         return false;
@@ -23,6 +42,7 @@ bool Copter::stabilize_init(bool ignore_checks)
 // should be called at 100hz or more
 void Copter::stabilize_run()
 {
+
     float target_roll, target_pitch;
     float target_yaw_rate;
     float pilot_throttle_scaled;
@@ -43,6 +63,14 @@ void Copter::stabilize_run()
     // To-Do: convert get_pilot_desired_lean_angles to return angles as floats
     get_pilot_desired_lean_angles(channel_roll->control_in, channel_pitch->control_in, target_roll, target_pitch, aparm.angle_max);
 
+    
+    //-------------------------------------------------------------------------------------------------------------------------------------------
+    //kd should just change target_roll, and target_pitch with adc input  (need to know a good scaling factor). 
+    adcpitch = ch1->voltage_average(); 
+    target_pitch = target_pitch + adcpitch*convfactor*criticalfactor;
+    //target_roll = target_roll + adcroll*cfactor;
+    //---------------------------------------------------------------------------------------------------------------------------------------------
+    
     // get pilot's desired yaw rate
     target_yaw_rate = get_pilot_desired_yaw_rate(channel_yaw->control_in);
 
